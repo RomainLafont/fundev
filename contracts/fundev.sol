bc721e67c142509d4177dd23d1d7572ec71d4041
Aristide Bayard <aristide.bayard@moonce.xyz>
2024-11-16 14:10:45 +0100

    contracts: add stuff for foundry

--- contracts/fundev.sol
+++ contracts/fundev.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract FunDev is Ownable(msg.sender) {
     uint256 public constant VALIDATOR_FEE_PERCENTAGE = 1;  // 1%
